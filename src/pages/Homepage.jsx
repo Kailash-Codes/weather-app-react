fc928b8bfd0afe1eebb1afacdc718db677a9e08c
kailash <dhunganak1920@gmail.com>
2023-02-21 12:10:50 +0545

    add loading spinner

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,17 +3,22 @@ import { ApiKey } from "../config/ApiKey";
 import getWeather from "../feature/getWeather";
 import { useLocation } from "../hooks/useLocation";
 import WeatherMainContainer from "../components/WeatherMainContainer";
-import { Button, TextField } from "@mui/material";
+import { Button, CircularProgress, TextField, Typography } from "@mui/material";
 import { Search } from "@mui/icons-material";
 import { isError, useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { Box } from "@mui/system";
 
 const Homepage = () => {
   const [currentLocation, isLocationLoading, isLocationError] = useLocation();
   const [searchLocation, setSearchLocation] = useState("thapathali");
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    refetch();
+    try {
+      await refetch();
+    } catch (error) {
+      alert("Unknown location. Please enter a valid location.");
+    }
   }
   function showError() {
     alert("location error");
@@ -25,66 +30,112 @@ const Homepage = () => {
     isError: isWeatherError,
   } = useQuery({
     queryKey: ["weather-data"],
-    queryFn: ()=>{
-     return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=${ApiKey}`)
-      .then((res) => res.data)
-      .catch((e) => e.message)
-    }
+    queryFn: () => {
+      return axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=${ApiKey}`
+        )
+        .then((res) => res.data)
+        .catch((e) => e.message);
+    },
   });
 
   return (
     <>
       {isLocationLoading ? (
-        "loading Location"
+        <Box mt={20}>
+       <center>
+        <CircularProgress />
+       </center>
+        </Box>
       ) : isLocationError ? (
         "location error"
       ) : isWeatherLoading ? (
-        "Weather Loading"
+        <Box mt={20}>
+       <center>
+        <CircularProgress />
+       </center>
+        </Box>
       ) : isWeatherError ? (
         "Weather Error"
       ) : (
-
-        <center>
-          <div className="text-center bg-blend-luminosity bg-[url('./assets/images/background.webp')] bg-cover object-cover my-10 lg:w-[800px] py-10 rounded-xl  px-5 ">
-            <form action="" onSubmit={handleSubmit}>
-              <div className="flex gap-5">
-                <TextField
-                  placeholder="Enter location..."
-                  variant="standard"
-                  sx={{
-                    padding: "0 10px",
-                    backgroundColor: "white",
-                    borderRadius: "10px",
-                    height: "40px",
-                  }}
-                  inputProps={{
-                    style: {
-                      padding: "9px 0", // adjust this value to change the position of the underline
-                    },
-                  }}
-                  onChange={(e) => setSearchLocation(e.target.value)}
-                  fullWidth
-                />
-                <Button type="submit" variant="contained" size="small">
-                  <Search />
-                </Button>
-              </div>
-            </form>
-            {/* //weather data if availabe => weatherData */}
-            <div>
-              <WeatherMainContainer
-                mainTemp={weatherData.main.temp}
-                weatherDesc={weatherData.weather[0].description}
-                placeName={weatherData.name}
-                weatherIcon={weatherData.weather[0].icon}
-                minAndMaxTemp={{
-                  min: weatherData.main.temp_min,
-                  max: weatherData.main.temp_max,
-                }}
-              />
+        <div>
+          {!weatherData?.main ? (
+            <div className="text-center bg-blend-luminosity bg-[url('./assets/images/background.webp')] bg-cover object-cover my-10 lg:w-[800px] py-10 rounded-xl  px-5 ">
+              <form action="" onSubmit={handleSubmit}>
+                <div className="flex gap-5">
+                  <TextField
+                    placeholder="Enter location..."
+                    variant="standard"
+                    sx={{
+                      padding: "0 10px",
+                      backgroundColor: "white",
+                      borderRadius: "10px",
+                      height: "40px",
+                    }}
+                    inputProps={{
+                      style: {
+                        padding: "9px 0", // adjust this value to change the position of the underline
+                      },
+                    }}
+                    onChange={(e) => setSearchLocation(e.target.value)}
+                    fullWidth
+                  />
+                  <Button type="submit" variant="contained" size="small">
+                    <Search />
+                  </Button>
+                </div>
+              </form>
+              <Box p={5}>
+                <Typography variant="h4" color={"white"}>
+                  The location is not available. Try searching for another
+                </Typography>
+              </Box>
             </div>
-          </div>
-        </center>
+          ) : (
+            <center>
+              <div className="text-center bg-blend-luminosity bg-[url('./assets/images/background.webp')] bg-cover object-cover my-10 lg:w-[800px] py-10 rounded-xl  px-5 ">
+                <form action="" onSubmit={handleSubmit}>
+                  <div className="flex gap-5">
+                    <TextField
+                      placeholder="Enter location..."
+                      variant="standard"
+                      sx={{
+                        padding: "0 10px",
+                        backgroundColor: "white",
+                        borderRadius: "10px",
+                        height: "40px",
+                      }}
+                      inputProps={{
+                        style: {
+                          padding: "9px 0", // adjust this value to change the position of the underline
+                        },
+                      }}
+                      onChange={(e) => setSearchLocation(e.target.value)}
+                      fullWidth
+                    />
+                    <Button type="submit" variant="contained" size="small">
+                      <Search />
+                    </Button>
+                  </div>
+                </form>
+                {/* //weather data if availabe => weatherData */}
+                <div>
+                  <WeatherMainContainer
+                    mainTemp={weatherData.main.temp}
+                    weatherDesc={weatherData.weather[0].description}
+                    placeName={weatherData.name}
+                    weatherIcon={weatherData.weather[0].icon}
+                    minAndMaxTemp={{
+                      min: weatherData.main.temp_min,
+                      max: weatherData.main.temp_max,
+                    }}
+                  />
+                </div>
+              </div>
+            </center>
+          )}
+        </div>
       )}
     </>
   );
